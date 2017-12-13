9efbaad23e510c6a1f531272944c134bb4ed7e84
heathher <33421881+heathher@users.noreply.github.com>
2017-12-13 13:07:15 +0300

    Update redb2d-mpi.c

--- redb2d-mpi.c
+++ redb2d-mpi.c
@@ -1,155 +1,204 @@
-#include <math.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 #include <sys/time.h>
 #include <mpi.h>
-#define  Max(a,b) ((a)>(b)?(a):(b))
-
-#define  N   1000
-double   maxeps = 0.1e-7;
-int itmax = 1000;
-MPI_Status status[4];
-int ll, shift;
+ 
+#define eps 0.1e-7
 double w = 0.5;
-double eps;
+
 MPI_Request req[4];
+MPI_Status status;
 
-double A [N][N];
+int myrank, ranksize;
+int startcol, lastcol, nextrank, prevrank;
 
-void relax();
-void init();
-void verify(); 
+int N;
+double **A;
+double **buff;
+double *buf[4];
 
+void range(int n1, int n2, int nprocs, int myrank, int *startrow, int *lastrow);
+void exchange(int phase);
+void verify();
 
-int main(int argc, char **argv)
-{
-	int myrank, ranksize;
-	int it;
-	double t1, t2;
-	int startrow, lastrow, nrow;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	MPI_Comm_size(MPI_COMM_WORLD, &ranksize);
-	MPI_Barrier(MPI_COMM_WORLD);
-	startrow = (myrank * N)/ ranksize;
-	lastrow = (((myrank + 1) * N) / ranksize) - 1;
-	nrow = lastrow - startrow + 1;
-
-	//printf("0");
-	//printf("%i %i\n", nrow, startrow);
-	if (myrank == 0)
-		t1 = MPI_Wtime();
-
-	//printf("*\n");
-	//printf("4\n");
-	init();
-	//printf("3\n");
-
-	//printf("*\n");
-	for(it = 1; it <= itmax; it++){
-		eps = 0.;
-
-		//printf("*\n");
-		relax(nrow, myrank, ranksize);
-		//printf( "it=%4i   eps=%f\n", it,eps);
-		if (eps < maxeps) 
-			break;
-	}
-
-	verify();
-	MPI_Barrier(MPI_COMM_WORLD);
-	if (myrank == 0){
-		t2 = MPI_Wtime();
-		printf("%d: Time of task = %lf\n", myrank, t2 - t1);
-	}
-	MPI_Finalize();
-	return 0;
+void init(double **A, int N) {
+ 
+    for (int i = 0; i <= N - 1; i++){
+        for (int j = 0; j <= N - 1; j++){
+            if (i == 0 || i == N - 1 || j == 0 || j == N - 1)
+                A[i][j]= 0.;
+            else
+                A[i][j]= (1. + i + j) ;
+        }
+    }
 }
 
-
-void init()
-{ 
-	for(int i = 0; i <= N-1; i++){
-		for(int j = 0; j <= N-1; j++){
-			if(i == 0 || i == N-1 || j == 0 || j == N-1){ 
-				A[i][j]= 0.;
-			} else {
-				A[i][j]= ( 1. + i + j) ;
-			}
-		}
-	}
-} 
-
-
-void relax(int nrow, int myrank, int ranksize)
-{
-	//printf("1\n");
-
-	//printf("ss%d \n",nrow);
-	for(int i = 1; i <= nrow-2; i++){
-		if (((i==1)&&(myrank==0)) || ((i==nrow) &&(myrank == ranksize -1)))	
-				continue;
-		for(int j = 1 + i % 2; j <= N-2; j+=2){
-			double b;
-			b = w*((A[i-1][j]+A[i+1][j]+A[i][j-1]+A[i][j+1])/4. - A[i][j]);
-			eps =  Max(fabs(b),eps);
-			A[i][j] = A[i][j] + b;
-		}
-	}
-	//printf("2\n");
-	//printf("1\n");
-	if (myrank != 0)
-		MPI_Irecv(&A[0][0],N,MPI_DOUBLE, myrank-1,1000,MPI_COMM_WORLD, &req[0]);
-
-	//printf("2\n");
-	if (myrank != ranksize - 1)
-		MPI_Isend(&A[nrow][0],N,MPI_DOUBLE, myrank+1,1000,MPI_COMM_WORLD, &req[2]);
-
-
-	//printf("3\n");
-
-	if (myrank != ranksize - 1)
-		MPI_Irecv(&A[nrow+1][0],N,MPI_DOUBLE, myrank+1,1001,MPI_COMM_WORLD, &req[3]);
-
-	//printf("4\n");
-
-	if (myrank != 0)
-		MPI_Isend(&A[1][0],N,MPI_DOUBLE, myrank-1,1001,MPI_COMM_WORLD, &req[1]);
-
-	ll = 4;
-	shift = 0;
-	if (myrank == 0){ 
-		ll = 2; 
-		shift = 2; 
-	}
-	if (myrank == ranksize - 1){ 
-		ll = 2; 
-	}
-	//MPI_Waitall(ll, &req[shift], &status[0]);
-
-
-	for(int i = 1; i <= nrow-2; i++){
-		if (((i==1) && (myrank == 0)) || ((i == nrow) && (myrank == ranksize -1))) 
-			continue;
-		for(int j = 1 + (i + 1) % 2; j<=N-2; j+=2){
-			double b;
-			b = w*((A[i-1][j]+A[i+1][j]+A[i][j-1]+A[i][j+1])/4. - A[i][j]);
-			A[i][j] = A[i][j] + b;
-		}
-	}
-
+void matrixAlloc(double ***A, int N) {
+    int i;
+    double** tmpA = (double**)malloc(N*sizeof(double*));
+    for (i = 0; i < N; i++)
+        tmpA[i] = (double*)malloc(N*sizeof(double));
+    *A = tmpA;
 }
-
-
-void verify()
-{ 
-	double s;
-
-	s=0.;
-	for(int i=0; i<=N-1; i++){
-		for(int j=0; j<=N-1; j++){
-			s=s+A[i][j]*(i+1)*(j+1)/(N*N);
-		}
+ 
+void bufAlloc() {
+	for (int i = 0; i < 4; ++i){
+		buf[i] = (double *)malloc(N * sizeof (double));
 	}
-	//printf("  S = %f\n",s);
+}
+ 
+int main (int argc, char **argv) {
+    
+    double newA, r, w, stopdiff, maxdiff, diff;
+    int i, j, phase, iteration;
+    double start, end;
+ 
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ranksize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+ 
+    N = atoi(argv[1]);
+ 
+    stopdiff = eps;
+ 
+    matrixAlloc(&A, N);
+    matrixAlloc(&buff, N);
+    init(A, N);
+    bufAlloc();
+ 
+    range(0, N, ranksize, myrank, &startcol, &lastcol);
+ 
+    nextrank = myrank + 1;
+    prevrank = myrank - 1;
+ 
+    if (nextrank == ranksize)
+        nextrank = MPI_PROC_NULL;
+ 
+    if (prevrank == -1)
+        prevrank = MPI_PROC_NULL;
+ 
+    if (myrank == 0) {
+        start = MPI_Wtime();
+    }
+ 
+    do {
+        maxdiff = 0.0;
+        for (phase = 0; phase < 2; phase++){
+            exchange(phase);
+            for (i = 1 ; i < N - 1 ; i++) {
+                for (j = startcol + (!(i & 1) ^ phase); j <= lastcol ; j += 2) {
+                    newA = (A[i-1][j] + A[i+1][j] + A[i][j-1] + A[i][j+1]) / 4.0;
+                    diff = fabs(newA - A[i][j]);
+                    if (diff > maxdiff)
+                        maxdiff = diff;
+                    A[i][j] = A[i][j] + w * (newA - A[i][j]);
+                }
+            }
+        }
+        MPI_Allreduce(&maxdiff, &diff, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+    } while (maxdiff < stopdiff);
+ 
+    if(myrank == 0) {
+        end = MPI_Wtime();
+        printf("%f\n", end - start);
+        verify();
+    }
+ 
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
+ 
+    return 0;
+}
+ 
+
+ 
+
+ 
+void range(int n1, int n2, int nprocs, int myrank, int *startrow, int *lastrow) {
+ 
+    float iwork1 = (n2 - n1 + 1) / nprocs;
+    float iwork2 = (n2 - n1 + 1) % nprocs;
+ 
+    int start, end;
+ 
+    start = (int) (myrank * iwork1 + n1 + (myrank < iwork2? myrank : iwork2));
+ 
+    end = (int) (start + iwork1 - 1);
+ 
+    if (iwork2 > myrank) {
+        end = end + 1;
+    }
+ 
+    *startrow = start;
+    *lastrow = end;
+}
+ 
+void exchange(int phase) {
+ 
+    int is1 = ((startcol + phase) % 2) + 1;
+    int is2 = ((lastcol + phase) % 2) + 1;
+    int ir1 = fabs(3 - is1);
+    int ir2 = fabs(3 - is2);
+    int icnt = 0;
+    int icnt1 = 0, icnt2 = 0;
+    int m = N - 1;
+    int i;
+ 
+    if (myrank != 0) {
+        icnt1 = 0;
+        for (i = is1; i <= m; i += 2) {
+            buf[0][icnt1] = A[i][startcol];
+            icnt1 = icnt1 + 1;
+        }
+    }
+ 
+    if (myrank != (ranksize - 1)) {
+        icnt2 = 0;
+        for (i = is2; i <= m; i += 2) {
+            buf[1][icnt2] = A[i][lastcol];
+            icnt2 = icnt2 + 1;
+        }
+    }
+ 
+    MPI_Isend(buf[0], icnt1, MPI_DOUBLE, prevrank, 1, MPI_COMM_WORLD, &req[0]);
+    MPI_Isend(buf[1], icnt2, MPI_DOUBLE, nextrank, 1, MPI_COMM_WORLD, &req[1]);
+ 
+    MPI_Wait(&req[0], &status);
+    MPI_Wait(&req[1], &status);
+ 
+    MPI_Irecv(buf[2], N, MPI_DOUBLE, prevrank, 1, MPI_COMM_WORLD, &req[2]);
+    MPI_Irecv(buf[3], N, MPI_DOUBLE, nextrank, 1, MPI_COMM_WORLD, &req[3]);
+ 
+   
+    MPI_Wait(&req[2], &status);
+    MPI_Wait(&req[3], &status);
+ 
+    if (myrank != 0) {
+        icnt = 0;
+        for (i = ir1; i <= m; i += 2) {
+            A[i][startcol - 1] = buf[2][icnt];
+            icnt = icnt + 1;
+        }
+    }
+ 
+    if (myrank != (ranksize - 1)) {
+        icnt = 0;
+        for (i = ir2; i <= m; i += 2) {
+            A[i][lastcol + 1] = buf[3][icnt];
+            icnt = icnt + 1;
+        }
+    }
+}
+ 
+void verify() {
+    double s;
+    s = 0.;
+    for(int i = 0; i <= N - 1; i++){
+        for(int j = 0; j <= N - 1; j++){
+            s = s + A[i][j] * (i + 1) * (j + 1) / (N * N);
+        }
+    }
+    printf("S = %f\n",s);
 }
